fdb8d9fd548b546a518653ae9c859b99bb0e74d9
Ace <ace.github@gmail.com>
2014-11-20 11:39:43 -0800

    Added 'published recently' field to admin

--- django_test/polls/admin.py
+++ django_test/polls/admin.py
@@ -1,4 +1,17 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+	"""Segregate fields in the admin interface - works better than vanilla 'fields'"""
+	fieldsets = [
+	(None, {'fields': ['question_text']}),
+	('Date information', {'fields':['pub_date'], 'classes':['collapse']}),] # Collapse class applies CSS for show/hide box
+	list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+	inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
